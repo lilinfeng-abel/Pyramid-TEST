412756edf57d54fe2dc0456f780c51b0d96fd35b
abel <lilinfeng.abel@gmail.com>
2013-05-16 22:11:14 +0800

    first commit
    Signed-off-by: abel <lilinfeng.abel@gmail.com>

---
+++ Helloworld/src/com/lilinfeng/test/SayHello.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:Helloworld
+ * File Name:SayHello.java
+ * Package Name:com.lilinfeng.test
+ * Date:May 16, 201310:11:40 PM
+ * Copyright (c) 2013, lilinfeng.abel@gmail.com All Rights Reserved.
+ *
+*/
+package com.lilinfeng.test;
+/**
+ * ClassName: SayHello <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON(可选). <br/>
+ * date: May 16, 2013 10:11:40 PM <br/>
+ *
+ * @author lilinfeng.abel@gmail.com
+ * @version 
+ * @since JDK 1.6
+ */
+public class SayHello {
+
+}
+
---
+++ Helloworld/src/com/lilinfeng/test/TestMain.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:Helloworld
+ * File Name:TestMain.java
+ * Package Name:com.lilinfeng.test
+ * Date:May 16, 201310:05:05 PM
+ * Copyright (c) 2013, lilinfeng.abel@gmail.com All Rights Reserved.
+ *
+*/
+package com.lilinfeng.test;
+/**
+ * ClassName: TestMain <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON(可选). <br/>
+ * date: May 16, 2013 10:05:05 PM <br/>
+ *
+ * @author lilinfeng.abel@gmail.com
+ * @version 
+ * @since JDK 1.6
+ */
+public class TestMain {
+	
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+
+}
+
